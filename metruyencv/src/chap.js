34efc2e28326ae3147908d44a37208be87aa7f04
SPINGPEACHVINH <daoxuan.vinh3@gmail.com>
2026-02-18 15:20:03 +0700

    Update metruyencv

--- metruyencv/src/chap.js
+++ metruyencv/src/chap.js
@@ -6,12 +6,8 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-        let paragraphs = doc.select(".text-left p");
-        let content = [];
-        for (let i = 0; i < paragraphs.size(); i++) {
-            content.push(paragraphs.get(i).text());
-        }
-        return Response.success(content.join("\n"));
+        let content = doc.select(".text-left").html();
+        return Response.success(content);
     }
     return null;
 }
