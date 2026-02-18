765242aba6badda1b86a38c68fe86a403d1fa382
SPINGPEACHVINH <daoxuan.vinh3@gmail.com>
2026-02-18 13:32:35 +0700

    Fix newline in chapter for metruyencv

--- metruyencv/src/chap.js
+++ metruyencv/src/chap.js
@@ -6,8 +6,12 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-        let content = doc.select(".text-left p").text();
-        return Response.success(content);
+        let paragraphs = doc.select(".text-left p");
+        let content = [];
+        for (let i = 0; i < paragraphs.size(); i++) {
+            content.push(paragraphs.get(i).text());
+        }
+        return Response.success(content.join("\n"));
     }
     return null;
 }
